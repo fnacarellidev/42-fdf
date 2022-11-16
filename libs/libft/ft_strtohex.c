06e5a4697eea505dc38d5c49177a4206a0e69102
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-11-16 20:19:51 -0300

    some additions to libft, including ft_printf

---
+++ libs/libft/ft_strtohex.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtohex.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 14:38:46 by fnacarel          #+#    #+#             */
+/*   Updated: 2022/11/08 17:12:49 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+long int	ft_hex_str_toi(char *str);
+
+long int	ft_hex_str_toi(char *str)
+{
+	int			i;
+	long int	res;
+
+	res = 0;
+	i = 0;
+	if (str[i])
+	{
+		while (str[i])
+		{
+			if (str[i] >= '0' && str[i] <= '9')
+				res = (res * 16) + (str[i] - '0');
+			else if (str[i] >= 'A' && str[i] <= 'F')
+				res = (res * 16) + (str[i] - 'A' + 10);
+			else if (str[i] >= 'a' && str[i] <= 'f')
+				res = (res * 16) + (str[i] - 'a' + 10);
+			i++;
+		}
+	}
+	return (res);
+}
