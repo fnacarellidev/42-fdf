b13d550b52bd623557c24af4bc67f45645385bcc
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-11-16 20:19:25 -0300

    get_next_line

---
+++ libs/get_next_line/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 18:22:56 by fnacarel          #+#    #+#             */
+/*   Updated: 2022/11/08 13:49:09 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# define BUFFER_SIZE 1
+
+size_t	ft_strlen_gnl(char *str);
+void	ft_strcpy(char *dest, const char *src);
+void	*ft_calloc_gnl(size_t nmemb, size_t size);
+char	*ft_strjoin_gnl(char *str, char *buf);
+char	*ft_strchr_gnl(char *s, int c);
+char	*fd_analysis(int fd, char *static_str);
+char	*get_line(char *str);
+char	*update_main_string(char *outdated_string);
+char	*get_next_line(int fd);
+
+#endif
---
+++ libs/get_next_line/get_next_line_utils.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 12:43:14 by fnacarel          #+#    #+#             */
+/*   Updated: 2022/11/08 14:01:03 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+size_t	ft_strlen_gnl(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (str)
+	{
+		while (str[i])
+			i++;
+		return (i);
+	}
+	return (0);
+}
+
+void	ft_strcat(char *dest, const char *src)
+{
+	int		i;
+	size_t	dest_len;
+
+	i = 0;
+	dest_len = ft_strlen_gnl(dest);
+	if (src)
+	{
+		while (*(src + i))
+		{
+			*(dest + dest_len + i) = *(src + i);
+			i++;
+		}
+	}
+}
+
+char	*ft_strjoin_gnl(char *str, char *buf)
+{
+	char	*finalstr;
+
+	if (!str)
+	{
+		str = malloc(1 * sizeof(char));
+		str[0] = '\0';
+	}
+	if (!str || !buf)
+		return (NULL);
+	finalstr = ft_calloc_gnl(sizeof(char), ft_strlen_gnl(str) + ft_strlen_gnl(buf) + 1);
+	if (!finalstr)
+		return (NULL);
+	ft_strcat(finalstr, str);
+	ft_strcat(finalstr, buf);
+	*(finalstr + ft_strlen_gnl(str) + ft_strlen_gnl(buf)) = '\0';
+	free(str);
+	return (finalstr);
+}
+
+char	*ft_strchr_gnl(char *s, int c)
+{
+	int			i;
+	const char	*first_occurence;
+
+	i = 0;
+	first_occurence = 0;
+	if (s)
+	{
+		while (*(s + i))
+		{
+			if (*(s + i) == (unsigned char) c)
+			{
+				first_occurence = (s + i);
+				return ((char *) first_occurence);
+			}
+			i++;
+		}
+		if (c == 0)
+			first_occurence = (s + ft_strlen_gnl(s));
+		return ((char *) first_occurence);
+	}
+	return (NULL);
+}
+
+void	*ft_calloc_gnl(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	i;
+
+	i = 0;
+	if (!nmemb || !size || (nmemb * size) / size != nmemb)
+		return (NULL);
+	ptr = (void *) malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	while (i < size * nmemb)
+	{
+		*((char *) ptr + i) = '\0';
+		i++;
+	}
+	return (ptr);
+}
