0ca7c2ddec774786d6c5de0fc6201b2135b06ebc
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-11-20 00:01:55 -0300

    set values updated, still not as good as i need to be

--- fdf_utils00.c
+++ fdf_utils00.c
@@ -6,87 +6,52 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 18:43:19 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/11/17 21:52:45 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/11/19 22:43:36 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fdf.h"
 
 void	set_values(t_pos **pos, t_str *str, char *file, t_vars *vars, t_map map)
 {
-	int	i;
-	int	j;
+	int	col;
+	int	row;
 	int	fd;
 
+	row = 0;
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		ft_close(vars);
-	i = 0;
 	while (1)
 	{
-		j = 0;
-		strings->gnl = get_next_line(fd);
-		if (strings->gnl == NULL)
+		col = 0;
+		str->gnl_ret = get_next_line(fd);
+		if (str->gnl_ret == NULL)
 			break ; // @TODO Not sure if this will work, have to worry about frees and stuff
-		strings->split_ret = ft_split(strings->gnl, ' ');
-		while (j < map.rows)
+		str->split_ret = ft_split(str->gnl_ret, ' ');
+		pos[row] = malloc(sizeof(t_pos) * map.columns);
+		while (col < map.columns)
 		{
-			pos[i][j] = malloc(sizeof(t_pos));
-			pos[i][j].x = i;
-			pos[i][j].y = j;
-			pos[i][j].z = ft_atoi(strings->split_ret[j]);
-			j++;
+			pos[row][col].x = col;
+			pos[row][col].y = row;
+			pos[row][col].z = ft_atoi(str->split_ret[col]);
+			ft_printf("pos[%d][%d].x = %d\npos[%d][%d].y = %d\npos[%d][%d].z = %d\n\n", row, col, pos[row][col].x, row, col, pos[row][col].y, row, col, pos[row][col].z);
+			col++;
 		}
-		i++;
+		free(str->gnl_ret);
+		free_split(str->split_ret);
+		row++;
 	}
 }
 
-int	split_size(char **str)
+void	free_split(char **splitted)
 {
 	int	i;
 
 	i = 0;
-	if (str)
+	while (splitted[i])
 	{
-		while (str[i])
-			i++;
-	}
-	return (i);
-}
-
-int	count_lines(char *argv)
-{
-	int	i;
-	int	fd;
-
-	i = 0;
-	fd = open(argv, O_RDONLY);
-	if (fd == -1)
-		exit(1);
-	while (get_next_line(fd))
+		free(splitted[i]);
 		i++;
-	close(fd);
-	return (i);
-}
-
-int	validate_input(char *argv)
-{
-	char	*str;
-
-	str = ft_strrchr(argv, '.');
-	if (ft_strlen(str) != 4)
-	{
-		ft_printf("File must be of .fdf extension.\n");
-		exit(1);
 	}
-	else if (ft_strlen(argv) == 4)
-	{
-		ft_printf("File can't be named only .fdf\n");
-		exit(1);
-	}
-	if (!(ft_strncmp(str, ".fdf", 4) == 0))
-	{
-		ft_printf("File must be of .fdf extension.\n");
-		exit(1);
-	}
-	return (1);
+	free(splitted);
 }
