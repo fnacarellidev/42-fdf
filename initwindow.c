ff283d7abc5362da690cefa4d0edf6e8cedb009d
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-11-20 00:02:30 -0300

    removed putimage and destroy image from function

--- initwindow.c
+++ initwindow.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 19:55:26 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/11/17 20:38:47 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/11/19 18:19:24 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fdf.h"
@@ -41,6 +41,4 @@ void	init_image(t_data *img, t_vars *vars)
 	img->img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
 	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, 
 			&img->line_length, &img->endian);
-	mlx_put_image_to_window(vars->mlx, vars->win, img->img, 0, 0);
-	mlx_destroy_image(vars->mlx, img->img);
 }
