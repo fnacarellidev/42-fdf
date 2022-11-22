be09072b39e4e6ce98a0b8d7755eafd5639f096d
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-11-21 21:53:14 -0300

    some macros that will need to me changed and norminette

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 12:13:55 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/11/19 23:59:56 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/11/21 21:51:46 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FDF_H
@@ -24,33 +24,36 @@
 # define WIDTH 1920
 # define HEIGHT 1080
 # define COLOR 0X2FFF00
+# define ESC_KEYCODE 65307
+# define ALPHA M_PI / 6
+# define THETA M_PI / 4
 
-typedef struct	s_vars
+typedef struct s_vars
 {
 	void	*mlx;
 	void	*win;
 }				t_vars;
 
-typedef struct	s_map
+typedef struct s_map
 {
 	int		rows;
 	int		columns;
 }				t_map;
 
-typedef struct	s_str
+typedef struct s_str
 {
 	char	*gnl_ret;
 	char	**split_ret;
 }				t_str;
 
-typedef struct	s_pos
+typedef struct s_pos
 {
 	int		x;
 	int		y;
 	int		z;
 }				t_pos;
 
-typedef struct	s_data
+typedef struct s_data
 {
 	void	*img;
 	char	*addr;
@@ -59,9 +62,11 @@ typedef struct	s_data
 	int		bits_per_pixel;
 }				t_data;
 
-void	ft_plot_line_high(t_pos *pos0, t_pos *pos1, t_data *data);
-void	ft_plot_line_low(t_pos *pos0, t_pos *pos1, t_data *data);
-void	ft_plot_line(t_pos *pos0, t_pos *pos1, t_data *data);
+void	multiply_matrix(t_pos **pos, t_map map);
+void	ft_plot_line_high(t_pos *pos0, t_pos *pos1, t_data *img);
+void	ft_plot_line_low(t_pos *pos0, t_pos *pos1, t_data *img);
+void	ft_plot_line(t_pos *pos0, t_pos *pos1, t_data *img);
+void	set_rows_and_columns(char *argv, t_map *map);
 void	set_rows(char *argv, t_map *map);
 void	set_columns(char *argv, t_map *map);
 void	free_split(char **splitted);
@@ -69,8 +74,11 @@ void	set_values(t_pos **pos, t_str *str, char *file, t_vars *vars, t_map map);
 void	init_window_and_hooks(t_vars *vars);
 void	init_image(t_data *img, t_vars *vars);
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+void	connect_both_axis(t_pos **pos, t_data *img, t_map map);
+void	init_loop_and_hooks(t_vars *vars);
+void	transform_positions(t_pos **pos, t_map map);
 int		count_lines(char *argv);
-int		number_of_columns(char **str);
+int		get_number_of_columns(char **str);
 int		validate_input(char *argv);
 int		ft_close(t_vars *vars);
 int		ft_keyhook(int keycode, t_vars *vars);
