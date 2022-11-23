ba557f8bab75c92b3ad00ca37bc49314f662af76
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-11-23 20:35:38 -0300

    backing this up, refactoring time

--- fdf_utils00.c
+++ fdf_utils00.c
@@ -6,27 +6,27 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 18:43:19 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/11/21 17:18:40 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/11/23 20:35:21 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fdf.h"
 
-void	set_values(t_pos **pos, t_str *str, char *file, t_vars *vars, t_map map)
+void	set_values(t_pos **pos, t_str *str, char *argv, t_map map)
 {
+	int	fd;
 	int	col;
 	int	row;
-	int	fd;
 
 	row = 0;
-	fd = open(file, O_RDONLY);
+	fd = open(argv, O_RDONLY);
 	if (fd == -1)
-		ft_close(vars);
+		exit(1);
 	while (1)
 	{
 		col = 0;
 		str->gnl_ret = get_next_line(fd);
 		if (str->gnl_ret == NULL)
-			break ; // @TODO Not sure if this will work, have to worry about frees and stuff
+			break ;
 		str->split_ret = ft_split(str->gnl_ret, ' ');
 		pos[row] = malloc(sizeof(t_pos) * map.columns);
 		while (col < map.columns)
@@ -37,40 +37,43 @@ void	set_values(t_pos **pos, t_str *str, char *file, t_vars *vars, t_map map)
 			col++;
 		}
 		free(str->gnl_ret);
-		free_split(str->split_ret);
+		free_matrix((void **)str->split_ret);
 		row++;
 	}
+	close(fd);
 }
 
 void	multiply_matrix(t_pos **pos, t_map map)
 {
 	int	row;
 	int	col;
+	int	val;
 
 	row = 0;
+	val = HEIGHT / sqrt(pow(map.columns, 2) + pow(map.rows, 2));
 	while (row < map.rows)
 	{
 		col = 0;
 		while (col < map.columns)
 		{
-			pos[row][col].x *= 10;
-			pos[row][col].y *= 10;
-			pos[row][col].z *= 10;
+			pos[row][col].x *= val / 1;
+			pos[row][col].y *= val / 1;
+			pos[row][col].z *= val / 2;
 			col++;
 		}
 		row++;
 	}
 }
 
-void	free_split(char **splitted)
+void	free_matrix(void **matrix)
 {
 	int	i;
 
 	i = 0;
-	while (splitted[i])
+	while (matrix[i])
 	{
-		free(splitted[i]);
+		free(matrix[i]);
 		i++;
 	}
-	free(splitted);
+	free(matrix);
 }
