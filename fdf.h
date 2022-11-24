9c97523a5aec4e5e492981d490c8c5289d7980f0
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-11-24 09:40:13 -0300

    stable version, works on any map and has colors!

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 12:13:55 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/11/24 00:21:31 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/11/24 09:16:37 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FDF_H
@@ -29,12 +29,8 @@
 # define J_KEYCODE 106
 # define K_KEYCODE 107
 # define L_KEYCODE 108
-
-typedef struct s_vars
-{
-	void	*mlx;
-	void	*win;
-}				t_vars;
+# define PLUS_KEYCODE 61
+# define MINUS_KEYCODE 45
 
 typedef struct s_map
 {
@@ -42,42 +38,49 @@ typedef struct s_map
 	int		columns;
 }				t_map;
 
-typedef struct s_str
-{
-	char	*gnl_ret;
-	char	**split_ret;
-}				t_str;
-
 typedef struct s_pos
 {
 	int		x;
 	int		y;
 	int		z;
+	int		color;
 }				t_pos;
 
-typedef struct s_data
+typedef struct s_win
 {
-	void	*img;
-	char	*addr;
-	int		endian;
-	int		line_length;
-	int		bits_per_pixel;
-}				t_data;
+	void		*mlx;
+	void		*win;
+	void		*img;
+	char		*addr;
+	int			endian;
+	int			line_length;
+	int			bits_per_pixel;
+	t_pos		**pos_ptr;
+	t_map		*map_ptr;
+}				t_win;
 
 void	validate_input(int argc, char *argv);
 void	multiply_matrix(t_pos **pos, t_map map);
-void	ft_plot_line(t_pos *pos0, t_pos *pos1, t_data *img);
+void	ft_plot_line(t_pos *pos0, t_pos *pos1, t_win *img);
 void	set_rows_and_columns(char *argv, t_map *map);
-void	set_values(t_pos **pos, t_str *str, int fd, t_map map);
-void	init_window(t_vars *vars);
-void	init_image(t_data *img, t_vars *vars);
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
-void	connect_both_axis(t_pos **pos, t_data *img, t_map map);
-void	init_loop_and_hooks(t_vars *vars);
+void	set_values(t_pos **pos, int fd, t_map map);
+void	init_window(t_win *win);
+void	init_image(t_win *img);
+void	my_mlx_pixel_put(t_win *data, int x, int y, int color);
+void	connect_both_axis(t_pos **pos, t_win *img, t_map map);
+void	init_loop_and_hooks(t_win *vars);
 void	transform_positions(t_pos **pos, t_map map);
 void	free_matrix(void **matrix);
+void	initialize_stuff(char *argv);
+void	ft_move_left(t_win *win);
+void	ft_move_down(t_win *win);
+void	ft_move_up(t_win *win);
+void	ft_move_right(t_win *win);
+void	put_img_to_window(t_win *win);
+void	zoom_in(t_win *win);
+void	zoom_out(t_win *win);
 int		count_lines(char *argv);
-int		ft_close(t_vars *vars);
-int		ft_keyhook(int keycode, t_vars *vars);
+int		ft_close(t_win *vars);
+int		ft_keyhook(int keycode, t_win *vars);
 
 #endif
--- main.c
+++ main.c
@@ -6,46 +6,24 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 19:05:51 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/11/24 00:21:16 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/11/24 08:52:47 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fdf.h"
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_win *win, int x, int y, int color)
 {
 	char	*dst;
 
 	if (x > WIDTH || y > HEIGHT || x < 0 || y < 0)
 		return ;
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = win->addr + (y * win->line_length + x * (win->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
 
 int	main(int argc, char **argv)
 {
-	int			fd;
-	t_data		img;
-	t_vars		vars;
-	t_str		*str;
-	t_map		*map;
-	t_pos		**pos;
-
-	fd = open(argv[1], O_RDONLY);
 	validate_input(argc, argv[1]);
-	str = malloc(sizeof(t_str));
-	map = malloc(sizeof(t_map));
-	set_rows_and_columns(argv[1], map);
-	pos = ft_calloc(sizeof(t_pos), map->rows);
-	set_values(pos, str, fd, *map);
-	init_window(&vars);
-	init_image(&img, &vars);
-	multiply_matrix(pos, *map);
-	transform_positions(pos, *map);
-	connect_both_axis(pos, &img, *map);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_destroy_image(vars.mlx, img.img);
-	free_matrix((void **)pos);
-	free(map);
-	free(str);
-	init_loop_and_hooks(&vars);
+	initialize_stuff(argv[1]);
+	return (0);
 }
